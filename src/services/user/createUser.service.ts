f575765c6eb277ee1c1f1593619bdd4a3df81018
Gabriel Larangeira <gabriel.larangeira.silva@hotmail.com>
2023-04-11 13:39:24 -0300

    feat: fix createUser

--- src/services/user/createUser.service.ts
+++ src/services/user/createUser.service.ts
@@ -1,28 +1,29 @@
-import AppDataSource from "../../data-source";
-import User from "../../entities/user";
-import { AppError } from "../../errors/AppError";
-import { IUserRequest } from "../../interfaces/user";
-import { userWithoutPasswordSerializer } from "../../serializers/user";
-
+import AppDataSource from '../../data-source';
+import User from '../../entities/user';
+import { AppError } from '../../errors/AppError';
+import { IUserRequest } from '../../interfaces/user';
+import { userWithoutPasswordSerializer } from '../../serializers/user';
 
 export const createUserService = async (data: IUserRequest) => {
-
-  const userRepository = AppDataSource.getRepository(User)
+  const userRepository = AppDataSource.getRepository(User);
 
   const emailVerify = await userRepository.findOneBy({
     email: data.email,
-  })
+  });
 
   if (emailVerify) {
-    throw new AppError('Email em uso', 409)
+    throw new AppError('Email already in use', 409);
   }
 
-  const userCreate = userRepository.create(data)
-  await userRepository.save(userCreate)
+  const userCreate = userRepository.create(data);
+  await userRepository.save(userCreate);
 
-  const userWithoutPassword = await userWithoutPasswordSerializer.validate(userCreate, {
-    stripUnknown: true
-  })
+  const userWithoutPassword = await userWithoutPasswordSerializer.validate(
+    userCreate,
+    {
+      stripUnknown: true,
+    }
+  );
 
-  return userWithoutPassword
-}
+  return userWithoutPassword;
+};
