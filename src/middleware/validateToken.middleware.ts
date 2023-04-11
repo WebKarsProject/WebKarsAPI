13c7afda5331016edbb45dbacb0952a1c4f7b178
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-11 12:53:17 -0300

    feat: add validate Token Middleware

---
+++ src/middleware/validateToken.middleware.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import { AppError } from '../errors/AppError';
+import 'dotenv/config';
+
+const validateTokenMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(' ')[1];
+
+  if (!token) {
+    throw new AppError('Invalid token', 401);
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded: any) => {
+    if (err) {
+      throw new AppError(err.message, 401);
+    }
+
+    req.user = {
+      id: decoded.sub,
+      email: decoded.email,
+    };
+
+    return next();
+  });
+};
+
+export default validateTokenMiddleware;
