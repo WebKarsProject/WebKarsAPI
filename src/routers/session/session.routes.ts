25c98fc17ba29ba155289599fe1f969c57c60350
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-11 11:19:15 -0300

    feat: session routes deleted

--- src/routers/session/session.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import sessionController from "../../controllers/session/session.controller";
-
-const sessionRoutes = Router();
-
-sessionRoutes.post("", sessionController);
-
-export default sessionRoutes;
