1a663a976a46e083983a8179bd32c64025b460d5
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-14 15:17:45 -0300

    feat: schema user

---
+++ src/schemas/user/index.ts
@@ -0,0 +1,38 @@
+import * as yup from "yup";
+import { SchemaOf } from "yup";
+import { IUserRequest, IUserResponse } from "../../interfaces/user";
+
+export const userSchemaReq: SchemaOf<IUserRequest> = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().min(6).required(),
+  cpf: yup.string().required(),
+  phone: yup.string().required(),
+  birthday: yup.date().required(),
+  description: yup.string().required(),
+  buyer: yup.boolean().required(),
+});
+
+export const userSchemaUpdate: SchemaOf<IUserRequest> = yup.object().shape({
+  name: yup.string().notRequired(),
+  email: yup.string().email().notRequired(),
+  password: yup.string().min(6).notRequired(),
+  cpf: yup.string().notRequired(),
+  phone: yup.string().notRequired(),
+  birthday: yup.date().notRequired(),
+  description: yup.string().notRequired(),
+  buyer: yup.boolean().notRequired(),
+});
+
+export const userSchemaReturned: SchemaOf<IUserResponse> = yup.object().shape({
+  id: yup.string().required(),
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  cpf: yup.string().required(),
+  phone: yup.string().required(),
+  birthday: yup.date().required(),
+  description: yup.string().required(),
+  buyer: yup.boolean().required(),
+});
+
+export const userSchemaList = yup.array(userSchemaReturned);
