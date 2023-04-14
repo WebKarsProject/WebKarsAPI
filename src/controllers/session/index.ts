e1ae097be5348a7fb87ae2883e995402240ca6bb
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-14 16:27:33 -0300

    fix: clear code

--- src/controllers/session/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-import sessionServices from "../../services/session/session.services";
-
-const sessionController = async (req: Request, res: Response) => {
-  const { token } = await sessionServices(req.body);
-  return res.status(200).json({ token });
-};
-
-export default sessionController;
