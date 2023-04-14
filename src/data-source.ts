54c3acac80bb124a24244819db0a4fbba4fa923b
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-14 16:26:59 -0300

    fix: data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,22 @@
-import { DataSource } from 'typeorm';
-import path from 'path';
-import 'dotenv/config';
-import User from './entities/user';
-import Vehicle from './entities/vehicle';
-import Address from './entities/address';
-import Image from './entities/image';
-import Comment from './entities/comments';
-import { CreateTables1681225781682 } from './migrations/1681225781682-createTables';
+import { DataSource } from "typeorm";
+import path from "path";
+import "dotenv/config";
+import User from "./entities/user";
+import Vehicle from "./entities/vehicle";
+import Address from "./entities/address";
+import Image from "./entities/image";
+import Comment from "./entities/comments";
 
 const AppDataSource = new DataSource(
-  process.env.NODE_ENV === 'test'
+  process.env.NODE_ENV === "test"
     ? {
-        type: 'sqlite',
-        database: ':memory:',
+        type: "sqlite",
+        database: ":memory:",
         synchronize: true,
-        entities: ['src/entities/*.ts'],
+        entities: ["src/entities/*.ts"],
       }
     : {
-        type: 'postgres',
+        type: "postgres",
         host: process.env.PGHOST,
         port: parseInt(process.env.PGPORT!),
         username: process.env.PGUSER,
@@ -26,7 +25,7 @@ const AppDataSource = new DataSource(
         logging: true,
         synchronize: false,
         entities: [User, Vehicle, Address, Image, Comment],
-        migrations: [CreateTables1681225781682],
+        migrations: [],
       }
 );
 
