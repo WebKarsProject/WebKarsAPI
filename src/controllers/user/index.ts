4745eb0ca92258430e902520ff9f8a7ae78864e9
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-14 15:41:25 -0300

    fix: clear code

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,19 +1,11 @@
 import { Request, Response } from "express";
-import { IUserRequest } from "../../interfaces/user";
 import { createUserService } from "../../services/user/createUser.service";
-import listUserService from "../../services/user/listUser.service";
 import retriveUserService from "../../services/user/retriveUser.service";
 import updateUserService from "../../services/user/updateUser.service";
-import deleteUserService from "../../services/user/deleteUser.service";
 
 export const createUserController = async (req: Request, res: Response) => {
   const user = await createUserService(req.body);
   return res.status(201).json(user);
-};
-
-export const listUserController = async (req: Request, res: Response) => {
-  const array = await listUserService();
-  return res.status(200).json(array);
 };
 
 export const retriverUserController = async (req: Request, res: Response) => {
@@ -24,9 +16,4 @@ export const retriverUserController = async (req: Request, res: Response) => {
 export const updateUserController = async (req: Request, res: Response) => {
   const data = await updateUserService(req.user.id, req.body);
   return res.status(200).json(data);
-};
-
-export const deleteUserController = async (req: Request, res: Response) => {
-  const data = await deleteUserService(req.user.id);
-  return res.status(204).json(data);
 };
--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import {
   createUserController,
-  deleteUserController,
-  listUserController,
   retriverUserController,
   updateUserController,
 } from "../../controllers/user";
@@ -11,7 +9,5 @@ import userExistMiddleware from "../../middleware/userExist.middleware";
 export const userRoutes = Router();
 
 userRoutes.post("", createUserController);
-userRoutes.get("", listUserController);
 userRoutes.get("/:id", userExistMiddleware, retriverUserController);
 userRoutes.patch("/:id", userExistMiddleware, updateUserController);
-userRoutes.delete("/:id", userExistMiddleware, deleteUserController);
