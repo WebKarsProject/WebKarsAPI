0b0d0ba7cf181d5a44194f1f832dd7794e9f1820
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-14 15:16:51 -0300

    fix: controller user

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,12 +1,28 @@
-import { Request, Response } from 'express';
-import { IUserRequest } from '../../interfaces/user';
-import { createUserService } from '../../services/user/createUser.service';
+import { Request, Response } from "express";
+import { IUserRequest } from "../../interfaces/user";
+import { createUserService } from "../../services/user/createUser.service";
 
-const createUserController = async (req: Request, res: Response) => {
-  const data: IUserRequest = req.body;
-  const user = await createUserService(data);
-
+export const createUserController = async (req: Request, res: Response) => {
+  const user = await createUserService(req.body);
   return res.status(201).json(user);
 };
 
-export { createUserController };
+export const listUserController = async (req: Request, res: Response) => {
+  const array = await listUserService();
+  return res.status(200).json(array);
+};
+
+export const retriverUserController = async (req: Request, res: Response) => {
+  const data = await retriveUserService(req.params.id);
+  return res.status(200).json(data);
+};
+
+export const updateUserController = async (req: Request, res: Response) => {
+  const data = await updateUserService(req.user.id, req.body);
+  return res.status(200).json(data);
+};
+
+export const deleteUserController = async (req: Request, res: Response) => {
+  const data = await deleteUserService(req.user.id);
+  return res.status(204).json(data);
+};
