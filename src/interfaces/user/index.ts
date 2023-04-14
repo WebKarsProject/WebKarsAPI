a3103d176cd461eba907b93b8680ac1df9dac78e
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-14 15:16:20 -0300

    fix: interface user

--- src/interfaces/user/index.ts
+++ src/interfaces/user/index.ts
@@ -1,27 +1,26 @@
 export interface IUserRequest {
-  name: string
-  email: string
-  cpf: string
-  phone: string
-  password: string
-  birthday: Date
-  description: string
-  buyer: boolean
-
+  name: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  password: string;
+  birthday: Date;
+  description: string;
+  buyer: boolean;
 }
 
 export interface IUserResponse {
-  name: string
-  email: string
-  cpf: string
-  phone: string
-  birthday: Date
-  description: string
-  buyer: boolean
-
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  birthday: Date;
+  description: string;
+  buyer: boolean;
 }
 
 export interface IUserLogin {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
