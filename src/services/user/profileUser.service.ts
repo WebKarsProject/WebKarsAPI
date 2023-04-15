e5073a2cd7d41764522eb450d2dc64fc6843569d
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-14 21:39:46 -0300

    fix: profile get service user

--- src/services/user/profileUser.service.ts
+++ src/services/user/profileUser.service.ts
@@ -1,16 +1,18 @@
 import AppDataSource from "../../data-source";
 import User from "../../entities/user";
 import { IUserRes } from "../../interfaces/user";
-import { userSchemaReturned } from "../../schemas/user";
+import { userSchemaRet } from "../../schemas/user";
 
 const profileUserService = async (id: string): Promise<IUserRes> => {
   const userRepository = AppDataSource.getRepository(User);
 
   const findUser = await userRepository.findOneBy({ id: id });
 
-  const validUser = await userSchemaReturned.validate(findUser, {
+  const validUser = await userSchemaRet.validate(findUser, {
     stripUnknown: true,
   });
+
+  console.log(validUser);
 
   return validUser;
 };
