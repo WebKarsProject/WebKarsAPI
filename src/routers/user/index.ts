cce3f7e5756aea906b5f8beb818369a37f5d3cae
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-17 15:25:33 -0300

    fix: routers users

--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -2,31 +2,31 @@ import { Router } from "express";
 import {
   createUserController,
   profileUserController,
-  updateAddressController,
   updateUserController,
 } from "../../controllers/user";
 import validateTokenMiddleware from "../../middleware/validateToken.middleware";
 import validateDataMiddleware from "../../middleware/validateData.middleware";
-import { userSchemaUpdate } from "../../schemas/user";
-import { AddressSchemaUpdate } from "../../schemas/address";
+import { userSchemaReq } from "../../schemas/user";
 
 export const userRoutes = Router();
 
 userRoutes.post(
   "",
-  validateDataMiddleware(userSchemaUpdate),
+  validateDataMiddleware(userSchemaReq),
   createUserController
 );
 userRoutes.get("", validateTokenMiddleware, profileUserController);
+
 userRoutes.patch(
   "",
-  validateDataMiddleware(userSchemaUpdate),
+  // validateDataMiddleware(userSchemaUpdate),
   validateTokenMiddleware,
   updateUserController
 );
-userRoutes.patch(
-  "/address",
-  validateDataMiddleware(AddressSchemaUpdate),
-  validateTokenMiddleware,
-  updateAddressController
-);
+
+// userRoutes.patch(
+//   "/address",
+//   validateDataMiddleware(AddressSchemaUpdate),
+//   validateTokenMiddleware,
+//   updateAddressController
+// );
