046c1d27182fe030b24fb049d131ea6c6a183d52
Alexson Pereira <alexspnpe@gmail.com>
2023-04-18 09:16:43 -0300

    fix: error handle

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import 'express-async-errors';
-import express, { Application } from 'express';
-import errorHandler from './errors/errorHandler';
-import cors from 'cors';
-import { userRoutes } from './routers/user';
-import sessionRoutes from './routers/session';
-import { vehicleRoutes } from './routers/vehicle';
+import 'express-async-errors'
+import express, { Application } from 'express'
+import errorHandler from './errors/errorHandler'
+import cors from 'cors'
+import { userRoutes } from './routers/user'
+import sessionRoutes from './routers/session'
+import { vehicleRoutes } from './routers/vehicle'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.use(errorHandler);
+app.use('/users', userRoutes)
+app.use('/session', sessionRoutes)
+app.use('/vehicle', vehicleRoutes)
 
-app.use('/users', userRoutes);
-app.use('/session', sessionRoutes);
-app.use('/vehicle', vehicleRoutes);
-export default app;
+app.use(errorHandler)
+export default app
