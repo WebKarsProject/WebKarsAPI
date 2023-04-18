f3b3d266a2213051dd9eee87c389d463a3ee49cd
Alexson Pereira <alexspnpe@gmail.com>
2023-04-18 09:18:37 -0300

    feat: add isBuyer in token sign

--- src/services/session/session.service.ts
+++ src/services/session/session.service.ts
@@ -1,31 +1,37 @@
-import AppDataSource from "../../data-source";
-import User from "../../entities/user";
-import { AppError } from "../../errors/AppError";
-import { compare } from "bcryptjs";
-import jwt from "jsonwebtoken";
-import "dotenv/config";
-import { IUserLogin } from "../../interfaces/user";
+import AppDataSource from '../../data-source'
+import User from '../../entities/user'
+import { AppError } from '../../errors/AppError'
+import { compare } from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+import { IUserLogin } from '../../interfaces/user'
 
 const sessionServices = async (data: IUserLogin) => {
-  const userRepository = AppDataSource.getRepository(User);
-  const user = await userRepository.findOneBy({ email: data.email });
+  const userRepository = AppDataSource.getRepository(User)
+  const user = await userRepository.findOneBy({ email: data.email })
 
   if (!user) {
-    throw new AppError("Email or password invalid", 403);
+    throw new AppError('Email or password invalid', 403)
   }
 
-  const passwordMatch = await compare(data.password, user.password);
+  const passwordMatch = await compare(data.password, user.password)
 
   if (!passwordMatch) {
-    throw new AppError("Email or password invalid", 403);
+    throw new AppError('Email or password invalid', 403)
   }
 
-  const token = jwt.sign({}, process.env.SECRET_KEY, {
-    subject: user.id,
-    expiresIn: "24h",
-  });
+  const token = jwt.sign(
+    {
+      isBuyer: user.buyer
+    },
+    process.env.SECRET_KEY,
+    {
+      subject: user.id,
+      expiresIn: '24h'
+    }
+  )
 
-  return { token };
-};
+  return { token }
+}
 
-export default sessionServices;
+export default sessionServices
