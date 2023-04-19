9c39328f510f7eaa36bc94115944a53945e1d734
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-19 15:57:21 -0300

    fix: migrate

--- src/migrations/1681928479254-create-table.ts
+++ src/migrations/1681928479254-create-table.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateTable1681815574574 implements MigrationInterface {
-    name = 'CreateTable1681815574574'
+export class CreateTable1681928479254 implements MigrationInterface {
+    name = 'CreateTable1681928479254'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "adresses" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "zipcode" character varying(8) NOT NULL, "city" character varying(100) NOT NULL, "street" character varying(100) NOT NULL, "state" character varying(2) NOT NULL, "number" character varying NOT NULL, "complement" character varying, CONSTRAINT "PK_2787c84f7433e390ff8961d552d" PRIMARY KEY ("id"))`);
