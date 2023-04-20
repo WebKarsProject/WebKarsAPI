612259a95cb4bbb3eff7d4c0068982e3f30d1c90
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-20 12:43:20 -0300

    fix: get vehicle user

--- src/services/vehicle/getVehicleUserService.service.ts
+++ src/services/vehicle/getVehicleUserService.service.ts
@@ -1,10 +1,21 @@
 import AppDataSource from "../../data-source";
 import User from "../../entities/user";
 import Vehicle from "../../entities/vehicle";
+import { AppError } from "../../errors/AppError";
 import { listVehicleUser } from "../../schemas/vehicles";
 
 export const getVehicleUserService = async (id: string) => {
   const vehicleRepository = AppDataSource.getRepository(Vehicle);
+  const userRepository = AppDataSource.getRepository(User);
+
+  const findUser = await userRepository.findOneBy({ id });
+
+  if (findUser.buyer) {
+    throw new AppError(
+      "it is not possible to list because the user is not an advertiser",
+      409
+    );
+  }
 
   const vehicles = await vehicleRepository.find({
     where: { user: { id: id } },
