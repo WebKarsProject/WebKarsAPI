70e80c0493fbfda9d22dce523c76a3965d8f83a6
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-20 13:31:04 -0300

    fix: add retriver user

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -23,3 +23,8 @@ export const updateAddressController = async (req: Request, res: Response) => {
   const data = await updateAddressService(req.user.id, req.body);
   return res.status(200).json(data);
 };
+
+export const retriverUserControler = async (req: Request, res: Response) => {
+  const data = await retriverUserService(req.user.id);
+  return res.status(200).json(data);
+};
