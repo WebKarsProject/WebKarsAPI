9338623b5ce1cabcc9b03c642c23091fc4921688
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-20 13:30:51 -0300

    fix: clear code

--- src/services/vehicle/getVehicle.service.ts
+++ src/services/vehicle/getVehicle.service.ts
@@ -1,8 +1,5 @@
 import AppDataSource from "../../data-source";
-import User from "../../entities/user";
 import Vehicle from "../../entities/vehicle";
-import { AppError } from "../../errors/AppError";
-import { listVehicleUser } from "../../schemas/vehicles";
 
 export const getVehicleService = async (id: string) => {
   const vehicleRepository = AppDataSource.getRepository(Vehicle);
