dea2f2e9e4005ceec9dfa295de84667dc0584648
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-20 16:58:38 -0300

    fix: add interface

--- src/interfaces/user/index.ts
+++ src/interfaces/user/index.ts
@@ -1,50 +1,62 @@
-import { IAddressReq, IAddressRes, IAddressUpdateReq } from '../address'
+import { IAddressReq, IAddressRes, IAddressUpdateReq } from "../address";
+import { IVehicleRetriveResponse } from "../vehicle";
 
 export interface IUserReq {
-  name: string
-  email: string
-  cpf: string
-  phone: string
-  password: string
-  birthday: Date
-  description: string
-  buyer: boolean
-  address: IAddressReq
+  name: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  password: string;
+  birthday: Date;
+  description: string;
+  buyer: boolean;
+  address: IAddressReq;
 }
 
 export interface IUserUpdateReq {
-  name?: string
-  email?: string
-  cpf?: string
-  phone?: string
-  password?: string
-  birthday?: Date
-  description?: string
-  buyer?: boolean
+  name?: string;
+  email?: string;
+  cpf?: string;
+  phone?: string;
+  password?: string;
+  birthday?: Date;
+  description?: string;
+  buyer?: boolean;
 }
 
 export interface IUserRes {
-  id: string
-  name: string
-  email: string
-  phone: string
-  birthday: Date
-  description: string
-  buyer: boolean
-  address: IAddressRes
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  birthday: Date;
+  description: string;
+  buyer: boolean;
+  address: IAddressRes;
+}
+
+export interface IUserRetrive {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  birthday: Date;
+  description: string;
+  buyer: boolean;
+  vehicle: IVehicleRetriveResponse;
 }
 
 export interface IUserNoAdressRes {
-  id: string
-  name: string
-  email: string
-  phone: string
-  birthday: Date
-  description: string
-  buyer: boolean
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  birthday: Date;
+  description: string;
+  buyer: boolean;
 }
 
 export interface IUserLogin {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
