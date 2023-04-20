07f1ca9f13efd5a78be409bf925ec5cd3897a6b4
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-19 22:21:49 -0300

    fix: service vehicle

--- src/services/vehicle/listAllVehicle.service.ts
+++ src/services/vehicle/listAllVehicle.service.ts
@@ -1,10 +1,12 @@
-import AppDataSource from '../../data-source';
-import Vehicle from '../../entities/vehicle';
+import AppDataSource from "../../data-source";
+import Vehicle from "../../entities/vehicle";
 
 export const listAllVehicleService = async () => {
   const vehicleRepository = AppDataSource.getRepository(Vehicle);
 
-  const vehicles = await vehicleRepository.find();
+  const vehicles = await vehicleRepository.find({
+    relations: { images: true },
+  });
 
   return vehicles;
 };
