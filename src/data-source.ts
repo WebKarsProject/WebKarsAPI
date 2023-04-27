bf70c3ce7a7841aeac620827fb65ef73770bbace
Alexson Pereira <alexspnpe@gmail.com>
2023-04-27 11:01:40 -0300

    fix: error migrations generate

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,22 @@
-import { DataSource } from "typeorm";
-import "dotenv/config";
-import User from "./entities/user";
-import Vehicle from "./entities/vehicle";
-import Address from "./entities/address";
-import Image from "./entities/image";
-import Comment from "./entities/comments";
-import { CreateTable1681928479254 } from "./migrations/1681928479254-create-table";
-import { ResetPassword1682476580759 } from "./migrations/1682476580759-resetPassword";
+import { DataSource } from 'typeorm'
+import 'dotenv/config'
+import User from './entities/user'
+import Vehicle from './entities/vehicle'
+import Address from './entities/address'
+import Image from './entities/image'
+import Comment from './entities/comments'
+import { Initial1682600983342 } from './migrations/1682600983342-initial'
 
 const AppDataSource = new DataSource(
-  process.env.NODE_ENV === "test"
+  process.env.NODE_ENV === 'test'
     ? {
-        type: "sqlite",
-        database: ":memory:",
+        type: 'sqlite',
+        database: ':memory:',
         synchronize: true,
-        entities: ["src/entities/*.ts"],
+        entities: ['src/entities/*.ts']
       }
     : {
-        type: "postgres",
+        type: 'postgres',
         host: process.env.PGHOST,
         port: parseInt(process.env.PGPORT!),
         username: process.env.PGUSER,
@@ -26,8 +25,8 @@ const AppDataSource = new DataSource(
         logging: true,
         synchronize: false,
         entities: [User, Vehicle, Address, Image, Comment],
-        migrations: [CreateTable1681928479254, ResetPassword1682476580759],
+        migrations: [Initial1682600983342]
       }
-);
+)
 
-export default AppDataSource;
+export default AppDataSource
