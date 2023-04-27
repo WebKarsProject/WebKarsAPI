05884b4d8487f1d775cd57a1273c565e806bad44
Gabriel Larangeira <gabriel.larangeira.silva@hotmail.com>
2023-04-27 12:58:00 -0300

    feat: fix update user

--- src/services/user/profileUser.service.ts
+++ src/services/user/profileUser.service.ts
@@ -1,14 +1,13 @@
-import AppDataSource from "../../data-source";
-import User from "../../entities/user";
-import { IUserRes } from "../../interfaces/user";
-import { userSchemaRet } from "../../schemas/user";
+import AppDataSource from '../../data-source';
+import User from '../../entities/user';
+import { IUserUpdateReq } from '../../interfaces/user';
+import { userSchemaRet } from '../../schemas/user';
 
-const profileUserService = async (id: string): Promise<IUserRes> => {
+const profileUserService = async (id: string): Promise<IUserUpdateReq> => {
   const userRepository = AppDataSource.getRepository(User);
 
   const findUser = await userRepository.findOne({
     where: { id: id },
-    relations: { address: true },
   });
 
   const validUser = await userSchemaRet.validate(findUser, {
