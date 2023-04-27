b9be1aa7a61d14bc768a65f21a05e83d6f12bb1e
Gabriel Larangeira <gabriel.larangeira.silva@hotmail.com>
2023-04-27 15:00:56 -0300

    feat: add delete user controller

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,11 +1,12 @@
-import { Request, Response } from "express";
-import { createUserService } from "../../services/user/createUser.service";
-import updateUserService from "../../services/user/updateUser.service";
-import profileUserService from "../../services/user/profileUser.service";
-import updateAddressService from "../../services/address/updateAddress.service";
-import retriverUserService from "../../services/user/retriveruser.service";
-import { resetPasswordEmailService } from "../../services/user/resetPasswordEmail.service";
-import { resetPasswordService } from "../../services/user/resetPassword.service";
+import { Request, Response } from 'express';
+import { createUserService } from '../../services/user/createUser.service';
+import updateUserService from '../../services/user/updateUser.service';
+import profileUserService from '../../services/user/profileUser.service';
+import updateAddressService from '../../services/address/updateAddress.service';
+import retriverUserService from '../../services/user/retriveruser.service';
+import { resetPasswordEmailService } from '../../services/user/resetPasswordEmail.service';
+import { resetPasswordService } from '../../services/user/resetPassword.service';
+import deleteUserService from '../../services/user/removeUser.service';
 
 export const createUserController = async (req: Request, res: Response) => {
   const user = await createUserService(req.body);
@@ -32,17 +33,23 @@ export const retriverUserControler = async (req: Request, res: Response) => {
   return res.status(200).json(data);
 };
 
+export const removeUserController = async (req: Request, res: Response) => {
+  const idUser = req.params.id;
+  await deleteUserService(idUser);
+  return res.status(204).json({});
+};
+
 export const sendResetPasswordEmailController = async (
   req: Request,
   res: Response
 ) => {
   const { email } = req.body;
   const { protocol } = req;
-  const host = req.get("host");
+  const host = req.get('host');
 
   await resetPasswordEmailService(email, protocol, host!);
 
-  return res.json({ message: "Reset token sent" });
+  return res.json({ message: 'Reset token sent' });
 };
 
 export const resetPasswordController = async (req: Request, res: Response) => {
@@ -51,5 +58,5 @@ export const resetPasswordController = async (req: Request, res: Response) => {
 
   await resetPasswordService(password, token);
 
-  return res.json({ message: "Password changed" });
+  return res.json({ message: 'Password changed' });
 };
