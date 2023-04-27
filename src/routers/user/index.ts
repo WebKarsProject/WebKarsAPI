028f178073913fb97eabad6558d8b371ba8228bc
Gabriel Larangeira <gabriel.larangeira.silva@hotmail.com>
2023-04-27 14:44:10 -0300

    Fix: route patch user

--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   createUserController,
   profileUserController,
@@ -7,37 +7,37 @@ import {
   sendResetPasswordEmailController,
   updateAddressController,
   updateUserController,
-} from "../../controllers/user";
-import validateTokenMiddleware from "../../middleware/validateToken.middleware";
-import validateDataMiddleware from "../../middleware/validateData.middleware";
-import { userSchemaReq, userSchemaUpdate } from "../../schemas/user";
-import { AddressSchemaUpdate } from "../../schemas/address";
+} from '../../controllers/user';
+import validateTokenMiddleware from '../../middleware/validateToken.middleware';
+import validateDataMiddleware from '../../middleware/validateData.middleware';
+import { userSchemaReq, userSchemaUpdate } from '../../schemas/user';
+import { AddressSchemaUpdate } from '../../schemas/address';
 
 export const userRoutes = Router();
 
 userRoutes.post(
-  "",
+  '',
   validateDataMiddleware(userSchemaReq),
   createUserController
 );
-userRoutes.get("", validateTokenMiddleware, profileUserController);
+userRoutes.get('', validateTokenMiddleware, profileUserController);
 
 userRoutes.patch(
-  "",
+  '/:id',
   validateDataMiddleware(userSchemaUpdate),
   validateTokenMiddleware,
   updateUserController
 );
 
 userRoutes.patch(
-  "/address",
+  '/address',
   validateDataMiddleware(AddressSchemaUpdate),
   validateTokenMiddleware,
   updateAddressController
 );
 
-userRoutes.post("/resetPassword", sendResetPasswordEmailController);
+userRoutes.post('/resetPassword', sendResetPasswordEmailController);
 
-userRoutes.patch("/resetPassword/:token", resetPasswordController);
+userRoutes.patch('/resetPassword/:token', resetPasswordController);
 
-userRoutes.get("/:id", retriverUserControler);
+userRoutes.get('/:id', retriverUserControler);
