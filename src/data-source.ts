1830aaa2fb6b5e23a9e261083aab3a97665be29b
Henrique Jesus <henrique.jesus128@gmail.com>
2023-05-02 14:51:02 -0300

    fix: data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { DataSource } from "typeorm";
 import "dotenv/config";
 import User from "./entities/user";
@@ -6,17 +5,7 @@ import Vehicle from "./entities/vehicle";
 import Address from "./entities/address";
 import Image from "./entities/image";
 import Comment from "./entities/comments";
-import { CreateTable1682608222499 } from "./migrations/1682608222499-create-table";
-=======
-import { DataSource } from 'typeorm';
-import 'dotenv/config';
-import User from './entities/user';
-import Vehicle from './entities/vehicle';
-import Address from './entities/address';
-import Image from './entities/image';
-import Comment from './entities/comments';
-import { Initial1683048161799 } from './migrations/1683048161799-initial';
->>>>>>> 9d6eb71d51651c7037b12423c157186d140cf704
+import { Initial1683048161799 } from "./migrations/1683048161799-initial";
 
 const AppDataSource = new DataSource(
   process.env.NODE_ENV === "test"
@@ -24,11 +13,7 @@ const AppDataSource = new DataSource(
         type: "sqlite",
         database: ":memory:",
         synchronize: true,
-<<<<<<< HEAD
         entities: ["src/entities/*.ts"],
-=======
-        entities: ['src/entities/*.ts'],
->>>>>>> 9d6eb71d51651c7037b12423c157186d140cf704
       }
     : {
         type: "postgres",
@@ -40,11 +25,7 @@ const AppDataSource = new DataSource(
         logging: true,
         synchronize: false,
         entities: [User, Vehicle, Address, Image, Comment],
-<<<<<<< HEAD
-        migrations: [CreateTable1682608222499],
-=======
         migrations: [Initial1683048161799],
->>>>>>> 9d6eb71d51651c7037b12423c157186d140cf704
       }
 );
 
