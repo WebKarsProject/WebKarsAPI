9aa6c216ae08b01e099f74613e826566bd47edcf
Alexson Pereira <alexspnpe@gmail.com>
2023-05-02 14:21:15 -0300

    fix : dependencias

--- src/services/user/resetPasswordEmail.service.ts
+++ src/services/user/resetPasswordEmail.service.ts
@@ -1,37 +1,27 @@
-import AppDataSource from "../../data-source";
-import { randomUUID } from "node:crypto";
-import User from "../../entities/user";
-import { emailService } from "../../utils/sendEmail.utils";
-import { AppError } from "../../errors/AppError";
+import AppDataSource from '../../data-source'
+import { randomUUID } from 'crypto'
+import User from '../../entities/user'
+import { emailService } from '../../utils/sendEmail.utils'
+import { AppError } from '../../errors/AppError'
 
-export const resetPasswordEmailService = async (
-  email: string,
-  protocol: string,
-  host: string
-) => {
-  const userRepository = AppDataSource.getRepository(User);
+export const resetPasswordEmailService = async (email: string, protocol: string, host: string) => {
+  const userRepository = AppDataSource.getRepository(User)
 
   const user = await userRepository.findOne({
-    where: { email: email },
-  });
+    where: { email: email }
+  })
 
   if (!user) {
-    throw new AppError("Invalid User", 404);
+    throw new AppError('Invalid User', 404)
   }
 
-  const resetToken = randomUUID();
+  const resetToken = randomUUID()
 
-  user.reset_token = resetToken;
+  user.reset_token = resetToken
 
-  await userRepository.save(user);
+  await userRepository.save(user)
 
-  const resetPasswordTemplate = emailService.resetPasswordTemplate(
-    email,
-    user.name,
-    protocol,
-    host,
-    resetToken
-  );
+  const resetPasswordTemplate = emailService.resetPasswordTemplate(email, user.name, protocol, host, resetToken)
 
-  await emailService.sendEmail(resetPasswordTemplate);
-};
+  await emailService.sendEmail(resetPasswordTemplate)
+}
