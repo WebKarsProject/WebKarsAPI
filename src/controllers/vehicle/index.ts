d003882bef827795db303157146864fa9edb8fa9
Henrique Jesus <henrique.jesus128@gmail.com>
2023-05-04 14:21:09 -0300

    fix: controller vehicle

--- src/controllers/vehicle/index.ts
+++ src/controllers/vehicle/index.ts
@@ -1,56 +1,63 @@
-import { Request, Response } from 'express'
-import { IVehicleRequest, IVehicleUpdate } from '../../interfaces/vehicle'
-import { createVehicleService } from '../../services/vehicle/createVehicle.service'
-import { listAllVehicleService } from '../../services/vehicle/listAllVehicle.service'
-import getVehicleUserService from '../../services/vehicle/getVehicleUserService.service'
-import { deleteVehicleService } from '../../services/vehicle/deleteVehicle.service'
-import { updateVehicleService } from '../../services/vehicle/updateVehicle.service'
-import getVehicleService from '../../services/vehicle/getVehicle.service'
+import { Request, Response } from "express";
+import { IVehicleRequest, IVehicleUpdate } from "../../interfaces/vehicle";
+import { createVehicleService } from "../../services/vehicle/createVehicle.service";
+import { listAllVehicleService } from "../../services/vehicle/listAllVehicle.service";
+import getVehicleUserService from "../../services/vehicle/getVehicleUserService.service";
+import { deleteVehicleService } from "../../services/vehicle/deleteVehicle.service";
+import { updateVehicleService } from "../../services/vehicle/updateVehicle.service";
+import getVehicleService from "../../services/vehicle/getVehicle.service";
 
 const createVehicleController = async (req: Request, res: Response) => {
-  const data = req.body
-  const idUser = req.user.id
+  const data = req.body;
+  const idUser = req.user.id;
 
-  const vehicle = await createVehicleService(data, idUser)
+  const vehicle = await createVehicleService(data, idUser);
 
-  return res.status(201).json(vehicle)
-}
+  return res.status(201).json(vehicle);
+};
 
 const listAllVehiclesController = async (req: Request, res: Response) => {
-  const vehicle = await listAllVehicleService(req)
+  const vehicle = await listAllVehicleService(req);
 
-  return res.json(vehicle)
-}
+  return res.json(vehicle);
+};
 
 const getVehicleUserController = async (req: Request, res: Response) => {
-  const vehicles = await getVehicleUserService(req.user.id)
-  return res.json(vehicles)
-}
+  const vehicles = await getVehicleUserService(req.params.id);
+  return res.json(vehicles);
+};
 
 const getVehicleController = async (req: Request, res: Response) => {
-  const vehicles = await getVehicleService(req.params.id)
-  return res.json(vehicles)
-}
+  const vehicles = await getVehicleService(req.params.id);
+  return res.json(vehicles);
+};
 
 const updateVehicleController = async (req: Request, res: Response) => {
-  const data: IVehicleUpdate = req.body
+  const data: IVehicleUpdate = req.body;
 
-  const vehicleId = req.params.id
+  const vehicleId = req.params.id;
 
-  const idUser = req.user.id
+  const idUser = req.user.id;
 
-  const updateVehicle = await updateVehicleService(data, vehicleId, idUser)
+  const updateVehicle = await updateVehicleService(data, vehicleId, idUser);
 
-  return res.status(201).json(updateVehicle)
-}
+  return res.status(201).json(updateVehicle);
+};
 
 const deleteVehicleController = async (req: Request, res: Response) => {
-  const idVehicle: string = req.params.id
-  const idUser = req.user.id
+  const idVehicle: string = req.params.id;
+  const idUser = req.user.id;
 
-  await deleteVehicleService(idVehicle, idUser)
+  await deleteVehicleService(idVehicle, idUser);
 
-  return res.status(204).json()
-}
+  return res.status(204).json();
+};
 
-export { createVehicleController, listAllVehiclesController, getVehicleUserController, updateVehicleController, deleteVehicleController, getVehicleController }
+export {
+  createVehicleController,
+  listAllVehiclesController,
+  getVehicleUserController,
+  updateVehicleController,
+  deleteVehicleController,
+  getVehicleController,
+};
