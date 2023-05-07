766e01efcecbf2c75b22486b51e5d63b4e72a3dc
Gabriel Larangeira <gabriel.larangeira.silva@hotmail.com>
2023-05-07 01:43:54 -0300

    feat: add patch in comment

--- src/routers/comment/index.ts
+++ src/routers/comment/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import {
   getCommentController,
   removeCommentController,
+  updateCommentController,
 } from '../../controllers/comment';
 
 const commentRoutes = Router();
 
 commentRoutes.get('/:id', getCommentController);
+commentRoutes.patch('/:id', updateCommentController);
 commentRoutes.delete('/:id', removeCommentController);
 
 export default commentRoutes;
